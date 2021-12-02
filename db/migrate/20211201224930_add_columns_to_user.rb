01b9da93af4871778f82f2afb772c19548079415
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-12-02 05:14:47 +0300

    fix migration

--- db/migrate/20211201224930_add_columns_to_user.rb
+++ db/migrate/20211201224930_add_columns_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUser < ActiveRecord::Migration[6.1]
   def change
-    add_reference :articles, :article, foreign_key: {to_table: :users}
-    add_reference :comments, :comment, foreign_key: {to_table: :users}
+    add_references :articles, :article, foreign_key: {to_table: :users}
+    add_references :comments, :comment, foreign_key: {to_table: :users}
   end
 end
--- db/migrate/20211202003527_add_user_field_to_comments.rb
+++ db/migrate/20211202003527_add_user_field_to_comments.rb
@@ -1,5 +1,5 @@
 class AddUserFieldToComments < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :user, foreign_key: {to_table: :users}
+    add_references :comments, :user, foreign_key: {to_table: :users}
   end
 end
