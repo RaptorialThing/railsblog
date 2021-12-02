9b0a83dd26213bac98c8b03ed04cc721af161ce5
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-12-02 05:41:27 +0300

    fix migration

--- db/migrate/20211201223650_add_user_to_articles.rb
+++ db/migrate/20211201223650_add_user_to_articles.rb
@@ -1,5 +1,5 @@
 class AddUserToArticles < ActiveRecord::Migration[6.1]
   def change
-    #add_reference :articles, :user, foreign_key: {to_table: :articles}
+    add_reference :articles, :user, foreign_key: {to_table: :users}
   end
 end
