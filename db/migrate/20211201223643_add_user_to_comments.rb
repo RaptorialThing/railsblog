5c7f292ba5ed7188132060418bdcfdedf9dd366d
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-12-02 05:32:56 +0300

    fix migration

--- db/migrate/20211201223643_add_user_to_comments.rb
+++ db/migrate/20211201223643_add_user_to_comments.rb
@@ -1,5 +1,5 @@
 class AddUserToComments < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :user, foreign_key: {to_table: :comments}
+    #add_reference :comments, :user, foreign_key: {to_table: :comments}
   end
 end
