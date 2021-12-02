5c18aba7907c1cf199f4c01154189a316de99f71
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2021-12-02 05:34:52 +0300

    fix migration

--- db/migrate/20211201191714_add_to_comments.rb
+++ db/migrate/20211201191714_add_to_comments.rb
@@ -1,5 +1,5 @@
 class AddToComments < ActiveRecord::Migration[6.1]
   def change
-    add_reference :comments, :article, foreign_key: {to_table: :articles}  
+    #add_reference :comments, :article, foreign_key: {to_table: :articles}  
   end
 end
